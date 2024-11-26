0d2304b9beaa7d18ef3bbd351e08c8b935b34b77
Josh Guo <joshuaguo2008@gmail.com>
2024-11-25 22:55:53 -0500

    Initialized repository

---
+++ README.md
@@ -0,0 +1,7 @@
+# project_template
+
+<a target="_blank" href="https://cookiecutter-data-science.drivendata.org/">
+    <img src="https://img.shields.io/badge/CCDS-Project%20template-328F97?logo=cookiecutter" />
+</a>
+
+Template for future data science projects
---
+++ src/utils.py
@@ -0,0 +1,8 @@
+import os
+import dotenv
+
+def load_env_vars():
+    """Load environment variables from .env"""
+    project_dir = os.path.join(os.path.dirname(__file__), os.pardir)
+    dotenv_path = os.path.join(project_dir, '.env')
+    dotenv.load_dotenv(dotenv_path)
