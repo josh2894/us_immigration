31be06bdce3b4ffd7ed654cb6eb5430410ae65e6
Josh Guo <joshuaguo2008@gmail.com>
2024-12-01 22:39:37 -0500

    Mostly finished main and cleaning

--- src/cleaning.py
+++ src/cleaning.py
@@ -7,7 +7,7 @@ def load_data():
     Load data from csv into dataframe
     
     Returns:
-        pd.DataFrame: loaded dataset
+        df (pd.DataFrame): loaded dataset
     """
     load_env_vars()
     us_immigration_csv = os.getenv("us_immigration_csv")
@@ -18,18 +18,32 @@ def load_data():
 def rename_raw_cols(df_raw: pd.DataFrame) -> pd.DataFrame:
     """
     Rename the columns from the raw data into conventional names
+    
     Args:
         df_raw (pd.DataFrame): raw dataframe loaded from csv
 
     Returns:
-        pd.DataFrame: renamed dataframe
+        df_renamed (pd.DataFrame): renamed dataframe
     """
-    df_raw.rename(columns={'Immigrants Obtaining Lawful Permanent Resident Status': 'lawful_permanent_resident_obt',
+    df_renamed = df_raw.rename(columns={'Immigrants Obtaining Lawful Permanent Resident Status': 'lawful_permanent_resident_obt',
                    'Refugee Arrivals': 'refugee_arrivals',
                    'Noncitizen Apprehensions': 'noncitizen_apprehensions',
                    'Noncitizen Removals': 'noncitizen_removals',
-                   'Noncitizen Returns': 'noncitizen_returns'}, inplace=True)
-    return df_raw
+                   'Noncitizen Returns': 'noncitizen_returns'})
+    return df_renamed
 
 
+def obj_to_int(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Converts the object columns into integer columns
     
+    Args:
+        df (pd.DataFrame): dataframe before data types are converted
+
+    Returns:
+        df (pd.DataFrame): dataframe with converted data types
+    """
+    df.replace(',', '', regex=True, inplace=True)
+    obj_cols = df.select_dtypes(include='object').columns.to_list()
+    df[obj_cols] = df[obj_cols].astype('int')
+    return df
--- src/main.py
+++ src/main.py
@@ -1,7 +1,18 @@
 import os
+from cleaning import load_data, rename_raw_cols, obj_to_int
 from utils import load_env_vars
-from cleaning import load_data, rename_raw_cols
 
-df = load_data()
-df = rename_raw_cols(df)
-print(df)
+def main():
+    load_env_vars()
+    
+    df = load_data()
+    df = rename_raw_cols(df)
+    df = obj_to_int(df)
+    
+    processed_data_dir = os.getenv('processed_data_dir')
+    destination_path = os.path.join(processed_data_dir, 'cleaned_us_immigration_data.csv')
+    df.to_csv(destination_path, index=False)
+    
+if __name__ == '__main__':
+    main()
+
